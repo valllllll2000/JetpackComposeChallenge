dbb3692da46d292695f60501b910a3f8b25cb762
valeria <valerie.cardoso@gmail.com>
2021-03-15 00:34:46 +0100

    updated colors

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -17,7 +17,7 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+val purple200 = Color(0xFFBDBDBD)
+val purple500 = Color(0xFF424242)
+val purple700 = Color(0xFF212121)
+val teal200 = Color(0xFFFF6D00)
